c7b00f9fc284623b758d85b3c9b6386e68538ed1
HO313 <wonik15@gmail.com>
2023-10-26 14:43:30 +0900

    weather v0.2 (JPA)

--- src/main/java/zerobase/weather/domain/Diary.java
+++ src/main/java/zerobase/weather/domain/Diary.java
@@ -1,6 +1,5 @@
 package zerobase.weather.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,15 +8,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Entity(name = "memo")
-public class Memo {
+public class Diary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String weather;
+    private String icon;
+    private double temperature;
     private String text;
+    private LocalDate date;
+
+
+
 }
